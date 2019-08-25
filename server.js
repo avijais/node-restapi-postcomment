3c046b9df5db5f34c7c2e88750b5c6aa23733043
avijais <abhi.jais@yahoo.co.in>
2019-08-25 20:29:52 +0530

    1st api created with get method

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express()
 
+app.get("/posts", (req, res) => {
+	res.send({
+		name: "Avinash"
+	})
+})
+
+app.get("/posts/fetch", (req, res) => {
+	res.send({
+		name: "post fetch"
+	})
+})
+
 app.listen(3000,() => {
 	console.log("server is running on port 3000")
 })
