4a26cf2dac040ce1135eaebafe6cfe3d5158757c
avijais <abhi.jais@yahoo.co.in>
2019-08-31 02:19:37 +0530

    installed body-parser and imported and used in project

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express()
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 
 // db connection
 require("./mongo")
 
 // model
 require("./model/Post")
+
+// Middleware
+app.use(bodyParser.json())
 
 const Post = mongoose.model("Post")
 
@@ -20,6 +24,7 @@ app.get("/", async (req, res) => {
 	}
 })
 
+// get post list
 app.get("/posts", async (req, res) => {
 	try {
 		const posts = await Post.find({})
@@ -28,6 +33,23 @@ app.get("/posts", async (req, res) => {
 		res.status(500)
 	}
 })
+
+// insert post
+app.post("/posts", async (req, res) => {
+	try {
+		console.log(req.body)
+		const post = new Post()
+		post.title = req.body.title
+		post.content = req.body.content
+		await post.save()
+		res.send(post)
+	} catch(error) {
+		console.log("error")
+		console.log(error)
+		res.status(500)
+	}
+})
+
 
 app.listen(3000,() => {
 	console.log("server is running on port 3000")
